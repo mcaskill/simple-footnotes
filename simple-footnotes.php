97b5c1f35b3478f77ad14fb2ef77a12106c62aae
nacin <nacin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-10 07:04:34 +0000

    Add missing dots.
    git-svn-id: https://plugins.svn.wordpress.org/simple-footnotes/trunk@262249 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- simple-footnotes.php
+++ simple-footnotes.php
@@ -23,7 +23,7 @@ class nacin_footnotes {
             $this->footnotes[$id] = array( 0 => false );
         $this->footnotes[$id][] = $content;
         $note = count( $this->footnotes[$id] ) - 1;
-        return ' <a class="simple-footnote" title="' . esc_attr( wp_strip_all_tags( $content ) ) . '" id="return-note-' $id . '-' . $note . '" href="#note-' $id . '-' . $note . '"><sup>' . $note . '</sup></a>';
+        return ' <a class="simple-footnote" title="' . esc_attr( wp_strip_all_tags( $content ) ) . '" id="return-note-' . $id . '-' . $note . '" href="#note-' . $id . '-' . $note . '"><sup>' . $note . '</sup></a>';
     }
 
     function the_content( $content ) {
@@ -32,7 +32,7 @@ class nacin_footnotes {
             return $content;
         $content .= '<div class="simple-footnotes"><p class="notes">Notes:</p><ol>';
         foreach ( array_filter( $this->footnotes[$id] ) as $num => $note )
-            $content .= '<li id="note-' . $id . '-' . $num . '">' . do_shortcode( $note ) . ' <a href="#return-note-' $id . '-' . $num . '">&#8617;</a></li>';
+            $content .= '<li id="note-' . $id . '-' . $num . '">' . do_shortcode( $note ) . ' <a href="#return-note-' . $id . '-' . $num . '">&#8617;</a></li>';
         $content .= '</ol></div>';
         return $content;
     }
